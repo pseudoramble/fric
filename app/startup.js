a3bd186fc116c14adf74cd8a0cabcb2129af1a35
Doug Swain <dswain@gmail.com>
2015-12-03 21:36:42 -0500

    Moved functions outside of the exported function from this module.

--- app/startup.js
+++ app/startup.js
@@ -1,19 +1,19 @@
+const isResetDay = (date) => {
+  const today = date && date.getDay ? date.getDay() : new Date().getDay();
+
+  return today === 1;
+};
+
+const doTheReset = () => {
+  const thingsToReset = JSON.parse(localStorage.getItem('budget-items') || '[]');
+  thingsToReset.forEach(function(thing) {
+    const details = JSON.parse(localStorage.getItem(thing));
+    details.transactions = [];
+    localStorage.setItem(thing, JSON.stringify(details));
+  });
+};
+
 export default () => {
-  const isResetDay = (date) => {
-    const today = date && date.getDay ? date.getDay() : new Date().getDay();
-
-    return today === 1;
-  };
-
-  const doTheReset = () => {
-    const thingsToReset = JSON.parse(localStorage.getItem('budget-items') || '[]');
-    thingsToReset.forEach(function(thing) {
-      const details = JSON.parse(localStorage.getItem(thing));
-      details.transactions = [];
-      localStorage.setItem(thing, JSON.stringify(details));
-    });
-  };
-
   if (isResetDay()) {
     doTheReset();
   }
